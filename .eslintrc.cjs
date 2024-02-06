2ac02e6042ae6f1947f64835c3f78005b12b5c83
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-02-06 16:43:09 -0500

    Init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'eslint-plugin-drizzle'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
+	rules: {
+		'curly': ['error', 'all'],
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{ ignoreRestSiblings: true, destructuredArrayIgnorePattern: '^_' },
+		],
+	},
+};
