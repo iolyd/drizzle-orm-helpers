581028a0fdb485a46cc38ebe891518906a0dc397
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-02-07 13:44:28 -0500

    Attempt fix typings and publishing

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	entry: ['src/index.ts'],
-	// entry: ['src/primitives.ts', 'src/custom-types.ts', 'src/utilities.ts'],
 	format: ['cjs', 'esm'],
-	splitting: true,
 	treeshake: true,
 	outDir: 'dist',
-	// dts: true,
-	experimentalDts: true,
+	dts: true,
+	// experimentalDts: true,
 	sourcemap: true,
 	clean: true,
 	bundle: true,
