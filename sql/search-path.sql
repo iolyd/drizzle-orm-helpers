046db78c9d7fea2ed3975e66618c8607dec95f77
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-03-05 12:16:33 -0500

    Add automatic approach for db search path customization

--- sql/search-path.sql
+++ sql/search-path.sql
@@ -7,9 +7,16 @@
  * with the extensions schema name.
  */
  
+-- Manual approach
 SHOW search_path;
-
+SELECT current_database();
+-- Paste values and run
 ALTER DATABASE [database name]
-SET search_path = "$user", public, extensions;
-
+SET search_path = [...search path], extensions;
 SHOW search_path;
+
+-- Automatic approach
+DO $$
+BEGIN
+EXECUTE 'ALTER DATABASE ' || current_database() || ' SET SEARCH_PATH = ' || current_setting('search_path') || ',extensions';
+END $$;
